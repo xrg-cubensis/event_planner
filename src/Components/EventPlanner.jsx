28c709f9c2c9334e851147bc8785abac4bf8b6f4
xdlp <sdlopezp@correo.udistrital.edu.co>
2025-04-25 19:16:50 -0500

    se añaden 5 secciones nuevas en JSX de EventPlanner

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -4,10 +4,25 @@ import './EventPlanner.css'; // Import CSS file for styling
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
+        <header>
+            <h1>Welcome to Event Planner</h1>
+        </header>
+        <section className="description">
+
+        </section>
+        <section className="events_categories">
+
+        </section>
+        <section className="features">
+
+        </section>
+        <section className="testimonials">
+
+        </section>
+        <section className="contact">
+
+        </section>
+    </div>
     );
 };
 
